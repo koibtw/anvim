0dcaaae38991a4c4897bfc5c8441bd96f3f9901d
june <me@koi.rip>
2026-03-27 13:57:31 +0100

    indent: meow

--- config/lua/anvim/init.lua
+++ config/lua/anvim/init.lua
@@ -13,3 +13,12 @@ vim.iter(vim.api.nvim_get_runtime_file("lua/anvim/plugins/*.lua", true)):each(fu
   require("lynn").import("anvim.plugins." .. name, true)
 end)
 require("lynn").setup()
+
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(ev)
+    if not vim.api.nvim_buf_is_loaded(ev.buf) then return end
+    local ok, _ = pcall(vim.treesitter.start, ev.buf)
+    if not ok then return end
+    vim.bo.indentexpr = "v:lua.require('anvim.indent').get_indent_line()"
+  end
+})

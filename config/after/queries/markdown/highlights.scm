e20baab562d73713227b52ef6287fa1477d77735
june <me@koi.rip>
2026-05-04 01:59:50 +0200

    checkedmark,checkedtext: init

---
+++ config/after/queries/markdown/highlights.scm
@@ -0,0 +1,7 @@
+;; extends
+
+(
+  (list_item
+    (task_list_marker_checked) @checkedmark
+    (_) @checkedtext)
+)
--- config/lua/anvim/autocmd.lua
+++ config/lua/anvim/autocmd.lua
@@ -9,3 +9,16 @@ vim.api.nvim_create_autocmd("ModeChanged", {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function()
+    local colors = require("evergarden.colors").get()
+
+    vim.api.nvim_set_hl(0, "@checkedmark.markdown", { fg = colors.overlay0 })
+    vim.api.nvim_set_hl(
+      0,
+      "@checkedtext.markdown",
+      { fg = colors.overlay1, strikethrough = true }
+    )
+  end,
+})

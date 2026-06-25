eb41cd9415f5d3470022356a453ce5794ffe1b35
june <me@koi.rip>
2026-06-25 12:32:14 +0200

    spell: init

---
+++ config/plugin/spell.lua
@@ -0,0 +1,22 @@
+if vim.g.loaded_anvim_spell then
+  return
+end
+
+vim.g.loaded_anvim_spell = true
+
+local spell_filetypes = {
+  "markdown",
+  "mail",
+  "jjdescription",
+  "html",
+  "gitcommit",
+  "nroff",
+  "groff",
+  "trinket",
+}
+
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function (ev)
+    vim.wo.spell = vim.tbl_contains(spell_filetypes, vim.bo[ev.buf].filetype)
+  end
+})

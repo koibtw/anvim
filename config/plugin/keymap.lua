5ef4c1e559083e4cbbf6785bd154b5c57a1e9b51
june <me@koi.rip>
2026-06-29 19:05:01 +0200

    keymap: failed attempt at fixing non-n motion

--- config/plugin/keymap.lua
+++ config/plugin/keymap.lua
@@ -24,7 +24,7 @@ local mappings = {
 }
 
 for rhs, lhs in pairs(mappings) do
-  vim.keymap.set("n", lhs, rhs, opts)
+  vim.keymap.set("", lhs, rhs, opts)
   vim.keymap.set("n", "<C-w>" .. lhs, "<C-w>" .. rhs, opts)
   vim.keymap.set("n", "<C-w>" .. lhs:upper(), "<C-w>" .. rhs:upper(), opts)
 end
@@ -32,6 +32,7 @@ end
 vim.keymap.set("n", "<C-w>k", "<C-w>n")
 vim.keymap.set("n", "<C-w>m", "<C-w>o")
 
+vim.keymap.set("", "f", "e", opts)
 vim.keymap.set("n", "t", "i", opts)
 vim.keymap.set("n", "k", "o", opts)
 vim.keymap.set("n", "h", "g", opts)
